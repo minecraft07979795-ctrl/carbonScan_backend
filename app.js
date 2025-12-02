c76a8ecc5bd4bbd104bcc4e692832ded6c989230
minecraft07979795-ctrl <minecraft07979795@gmail.com>
2025-12-02 23:10:53 +0530

    .

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ utility.app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 // }));
 
 utility.app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+  origin: [process.env.FRONTEND_URL, "http://localhost:3000","https://frontend-project-ab.vercel.app/login"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
